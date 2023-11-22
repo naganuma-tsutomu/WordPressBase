4dfe81d368abfb0ca217d0f22681269d0139e362
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2023-11-22 15:51:42 +0900

    update

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <?php wp_head() ?>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo get_bloginfo( 'name' ); ?></title>
+  </head>
+  <body>
+      <div id="app">
+        <p>j
+
+        </p>
+      </div>
+  </body>
+
+  <?php wp_footer() ?>
+</html>
